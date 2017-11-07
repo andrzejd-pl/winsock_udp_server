054649cc7c4602b3076bb4774b27b65f34c79cb6
Andrzej Dybowski <janjej@hotmail.com>
2017-11-07 16:17:26 +0100

    Add project files.

---
+++ Winsock_2_server/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "WSASession.h"
+#include "UDPSocket.h"
+
+int main() {
+	try {
+		WSASession Session;
+		UDPSocket Socket;
+		char buffer[100];
+
+		Socket.Bind(100);
+		while (1) {
+			sockaddr_in add = Socket.RecvFrom(buffer, sizeof(buffer));
+
+			std::string input(buffer);
+			std::reverse(std::begin(input), std::end(input));
+			Socket.SendTo(add, input.c_str(), input.size());
+		}
+	}
+	catch (std::system_error& e) {
+		std::cout << e.what();
+	}
+	return 0;
+}
