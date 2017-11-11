e6e019cbb7c5eff5f940cf0254d627b3353f5ea0
Andrzej Dybowski <janjej@hotmail.com>
2017-11-11 10:43:13 +0100

    add: - struct Packet; write on console address IP sender

--- Winsock_2_server/main.cpp
+++ Winsock_2_server/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include "WSASession.h"
 #include "UDPSocket.h"
+#include "Packet.h"
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "Ws2_32.lib")
 
 int main() {
 	try {
 		WSASession Session;
 		UDPSocket Socket;
-		char buffer[2];
+		char buffer[3];
 
 		Socket.Bind(100);
 		while (1) {
 			sockaddr_in add = Socket.RecvFrom(buffer, sizeof(buffer));
 
+			Packet packet(buffer, 2);
+			sockaddr* sock = reinterpret_cast<sockaddr *>(&add);
+			IN_ADDR addr = add.sin_addr;
+			char ip[20];
 
+			InetNtop(AF_INET, &(add.sin_addr.s_addr), ip, 20);
+
+			std::cout << "Packet from: " << ip << std::endl;
 
 			std::string input(buffer);
 			
