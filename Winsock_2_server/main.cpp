9f70794ed8b3333a293ed0da89707409e0c8ee10
Andrzej Dybowski <janjej@hotmail.com>
2017-11-19 16:35:29 +0100

    improve class Packet and Packet::PacketBuilder (remove fields: ack, error, overflow, bad_response, bad_client; and methods: setters and getters) according to documentacion my protocol

--- Winsock_2_server/main.cpp
+++ Winsock_2_server/main.cpp
@@ -17,36 +17,28 @@ int main() {
 			sockaddr_in add = Socket.RecvFrom(buffer, sizeof(buffer));
 
 			Packet packet(buffer, 2);
-			
+
 			/*read address ip sender's*/
-			
+
 			char ip[20];
 			InetNtop(AF_INET, &(add.sin_addr.s_addr), ip, 20);
 
 			/*-----------------*/
 
-			std::cout << "Packet from " << ip << " contains fields: \n\t" << "operation - " << packet.getOperation() <<
+			std::cout << "Packet from " << ip << " contains fields:" <<
+				"\n\t" << "operation - " << packet.getOperation() <<
 				"\n\t" << "response - " << packet.getResponse() <<
-				"\n\t" << "id - " << packet.getId() << 
-				"\n\t" << "ack - " << packet.getAck() <<
-				"\n\t" << "overflow - " << packet.getOverflow() <<
-				"\n\t" << "bad response - " << packet.getBadResponse() <<
-				"\n\t" << "bad client - " << packet.getBadClient() <<
+				"\n\t" << "id - " << packet.getId() <<
 				std::endl;
 
 			packet = Packet::PacketBuilder()
 				.set_operation(0)
 				.set_response(0)
-				.set_ack(1)
-				.set_bad_client(0)
-				.set_bad_response(0)
-				.set_error(0)
 				.set_id(0)
-				.set_overflow(0)
 				.build();
 
 			std::string input(packet.convertToSend());
-			
+
 			Socket.SendTo(add, input.c_str(), input.size());
 
 		}
