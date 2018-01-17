ec925f61980565d532cac2a860e257e7bba8feca
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-01-17 16:24:46 +0100

    add basic unit tests for Packet class

--- network_library_test/test.cpp
+++ network_library_test/test.cpp
@@ -44,4 +44,160 @@ BOOST_AUTO_TEST_CASE(ExpectBuilder) {
 
 	BOOST_TEST(p.convertToSend() == buff);
 }
+BOOST_AUTO_TEST_CASE(AckBuilder) {
+	int operation = 7, id = 1, response = 0;
+	Packet p = Packet::AckPacketBuilder().
+		set_id(id).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(AnswerBuilder) {
+	int operation = 5, id = 1, response = 7;
+	Packet p = Packet::AnswerPacketBuilder().
+		set_id(id).
+		set_response(response).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(GenerateIdBuilder) {
+	int operation = 0, id = 1, response = 0;
+	Packet p = Packet::GenerateIdPacketBuilder().
+		set_id(id).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(StartBuilder) {
+	int operation = 2, id = 1, response = 0;
+	Packet p = Packet::StartPacketBuilder().
+		set_id(id).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(CorrectAnswerBuilder) {
+	int operation = 6, id = 1, response = 2;
+	Packet p = Packet::CorrectAnswerPacketBuilder().
+		set_id(id).
+		set_response(response).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(EndBuilder) {
+	int operation = 15, id = 1, response = 0;
+	Packet p = Packet::EndPacketBuilder().
+		set_id(id).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(ResponseBuilder) {
+	int operation = 3, id = 1, response = 5;
+	Packet p = Packet::ResponsePacketBuilder().
+		set_id(id).
+		set_response(response).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
+BOOST_AUTO_TEST_CASE(AssayBuilder) {
+	int operation = 4, id = 1, response = 4;
+	Packet p = Packet::AssayPacketBuilder().
+		set_id(id).
+		set_response(response).
+		build();
+
+	std::string buff;
+	char element = 0;
+	element |= (operation << 4);
+	element |= (response << 1);
+	element |= (id >> 3);
+	buff.push_back(element);
+
+	element = 0;
+	element |= (id << 5);
+	buff.push_back(element);
+
+	BOOST_TEST(p.convertToSend() == buff);
+}
 BOOST_AUTO_TEST_SUITE_END();
